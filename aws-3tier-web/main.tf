7ce2ee5137edcc82b3773749336b4d405b58fc2e
seochan2 <seochan2@gmail.com>
2023-07-27 09:43:38 +0900

    Update main.tf

--- aws-3tier-web/main.tf
+++ aws-3tier-web/main.tf
@@ -1,1 +1,26 @@
+locals {
+  project_name = "tf-sample"
+}
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  name   = local.project_name
+  cidr   = "10.0.0.0/16"
+  region = "ap-southeast-2"
+
+  enable_dns_hostnames = true
+
+  azs              = ["a", "c"]
+  public_subnets   = ["10.0.10.0/24", "10.0.11.0/24"]
+  private_subnets  = ["10.0.50.0/24", "10.0.51.0/24"]
+  database_subnets = ["10.0.100.0/24", "10.0.101.0/24"]
+
+  single_nat_gateway = terraform.workspace == "stg" ? true : false
+
+  default_tags = {
+    TerraformManaged = true
+    Environment      = terraform.workspace
+    Project          = local.project_name
+  }
+}

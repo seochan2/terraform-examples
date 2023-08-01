d8fd53bbe04c857a19d39f0794ebac8422469f1c
seochan2 <seochan2@gmail.com>
2023-08-01 17:26:50 +0900

    Update outputs.tf

--- aws-3tier-web/modules/vpc/outputs.tf
+++ aws-3tier-web/modules/vpc/outputs.tf
@@ -1,1 +1,64 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
 
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = aws_vpc.this.default_security_group_id
+}
+
+output "default_network_acl_id" {
+  description = "The ID of the default network ACL"
+  value       = aws_vpc.this.default_network_acl_id
+}
+
+output "igw_id" {
+  description = "The ID of the internet gateway"
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_subnets_ids" {
+  description = "List of IDs of public subnets"
+  value       = ["${aws_subnet.public.*.id}"]
+}
+
+output "private_subnets_ids" {
+  description = "List of IDs of private subnets"
+  value       = ["${aws_subnet.private.*.id}"]
+}
+
+output "private_route_table_ids" {
+  description = "List of IDs of private route tables"
+  value       = ["${aws_route_table.private.*.id}"]
+}
+
+output "nat_ids" {
+  description = "List of allocation ID of Elastic IPs created for AWS NAT Gateway"
+  value       = ["${aws_eip.nat.*.id}"]
+}
+
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = ["${aws_eip.nat.*.public_ip}"]
+}
+
+output "natgw_ids" {
+  description = "List of NAT Gateway IDs"
+  value       = ["${aws_nat_gateway.this.*.id}"]
+}
+
+output "database_subnets_ids" {
+  description = "List of IDs of database subnets"
+  value       = ["${aws_subnet.database.*.id}"]
+}
+
+output "database_subnet_group_ids" {
+  description = "List of IDs of database subnet group"
+  value       = aws_db_subnet_group.database.*.id
+}

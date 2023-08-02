030b89531c25085fa5e57d8b5873b52a11ac5316
seochan2 <seochan2@gmail.com>
2023-08-02 21:39:57 +0900

    Update variables.tf

--- aws-3tier-web/modules/vpc/variables.tf
+++ aws-3tier-web/modules/vpc/variables.tf
@@ -1,1 +1,65 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = ""
+}
 
+variable "cidr" {
+  description = "The IPv4 CIDR block for the VPC"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "The name of the selected region"
+  type        = string
+  default     = ""
+}
+
+variable "enable_dns_hostname" {
+  description = "Should be true to enable DNS hostname in the VPC"
+  type        = bool
+  default     = true
+}
+
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "azs" {
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = false
+}
+
+variable "nat_gateway_destination_cidr_block" {
+  description = "Used to pass a custom destination route for private NAT Gateway. If not specified, the default 0.0.0.0/0 is used as a destination route"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "database_subnets" {
+  description = "A list of database subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+
+variable "default_tags" {
+  description = "A map of default tags to use on all resources"
+  type        = map(string)
+  default     = {}
+}
